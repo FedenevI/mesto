6390e2b01522a516981adfb82001260d611f42c9
FedenevI <fedenevia@yandex.ru>
2023-10-13 19:38:29 +0300

    pr9v1.2

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -6,7 +6,7 @@ export default class PopupWithForm extends Popup {
       this._submitFunc = submitFunc;
       this._form = this._popup.querySelector('.popup__form');
       this._input = this._form.querySelectorAll('.popup__input');
-      this.submitBtn = this._form.querySelector('.popup__save-button')
+      this.submitBtn = this._form.querySelector('.popup__save-button');
     }
 
     close() {
@@ -18,7 +18,7 @@ export default class PopupWithForm extends Popup {
       super.setEventListeners();
       this._form.addEventListener('submit', (event) => {
         event.preventDefault();
-        this.submitBtn.textContent = `${this.submitBtn.textContent}...`
+        this.submitBtn.textContent = `${this.submitBtn.textContent}...`;
         this._submitFunc(this._getInputValues());
       })
     }
